79b8e0a6484eb3e95d9ad6545e441187c54fc914
miguelmplpadilla <miguelpadillal@hotmail.es>
2022-12-09 03:59:08 +0100

    Animaciones

--- Bow_Plataformas/Assets/Resources/Scripts/Player/PlayerController.cs
+++ Bow_Plataformas/Assets/Resources/Scripts/Player/PlayerController.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rigidbody;
+    private Vector3 movement;
+    public float speed;
+
+    private Animator animator;
+
+    private GameObject camara;
+    
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        camara = GameObject.Find("Camara");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        movimiento();
+    }
+
+    private void movimiento()
+    {
+        float axisVertical = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(camara.transform.forward.z);
         
+        movement = new Vector3(0, 0,axisVertical * camara.transform.forward.z);
+
+        if (axisVertical > 0)
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float verticalVelocity = movement.normalized.z * speed;
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, verticalVelocity);
     }
 }
